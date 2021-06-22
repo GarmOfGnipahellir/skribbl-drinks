af97d248c05342a0805241beacf28eeef7e071a8
Henrik Melsom <melsom.henrik@gmail.com>
2021-06-23 01:09:54 +0200

    multi window testing works need to figure out which player is drawing

--- tests/e2e/index.js
+++ tests/e2e/index.js
@@ -1,36 +1,37 @@
 describe("skribbl drinks tests", function () {
-  before((browser) =>
+  before((browser) => {
     browser
       .url(browser.launchUrl)
       .waitForElementVisible("body")
       .click("#cmpwelcomebtncustom > a")
       .click(
         "#cmpbox > div.cmpboxinner > div.cmpboxbtns.cmpboxbtnscustomchoices > a.cmpboxbtn.cmpboxbtnyes.cmpboxbtnyescustomchoices"
-      )
-  );
+      );
+  });
 
   test("water mark is added", (browser) =>
     browser.assert.visible("#drinksWaterMark"));
 
   test("start new game", (browser) => {
-    browser.openNewWindow();
-
     browser
-      .windowHandles((result) => {
-        var handle = result.value[0];
-        browser.switchWindow(handle);
+      .click("#buttonLoginCreatePrivate")
+      .waitForElementVisible("#invite")
+      .getValue("#invite", (result) => {
+        browser
+          .openNewWindow()
+          .windowHandles((result) => browser.switchWindow(result.value[1]))
+          .url(result.value)
+          .click("#formLogin > button.btn.btn-success.btn-lg.btn-block");
       })
-      .url(browser.launchUrl)
-      .pause(1000);
-
-    browser
-      .windowHandles((result) => {
-        var handle = result.value[1];
-        browser.switchWindow(handle);
-      })
-      .url(browser.launchUrl)
-      .pause(1000);
+      .windowHandles((result) => browser.switchWindow(result.value[0]))
+      .waitForElementVisible("#player1")
+      .click("#buttonLobbyPlay")
   });
 
-  after((browser) => browser.end());
+  test("make wrong guess", (browser) => {
+    browser
+      .waitForElementVisible("#overlay > div > div.wordContainer > div:nth-child(1)");
+  });
+
+  after((browser) => browser.pause(1000000));
 });
